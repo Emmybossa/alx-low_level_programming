2cbed04960b8c0a70161dfa9294f4db1c6343209
emmybossa <emmybossa@gmail.com>
2023-09-23 04:29:20 -0700

    Pointers, arrays, and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -13,3 +16,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
+#endif
